dc4f2d7caaffee89f053a00840c2c78f7279d774
neetdev32 <neetdev32@proton.me>
2026-07-24 02:22:41 +0100

    Implement file pairing and processing script
    This script creates pairs of files from 'evil' and 'good' directories, runs a subprocess with 'collide.py', and manages file copying and deletion.

---
+++ make_pairs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import glob
+import secrets
+import shutil
+import subprocess
+
+random_name = lambda : secrets.token_hex()[:(7 + secrets.randbelow(12 - 7))] + ".png"
+pairs_dir = "pairs"
+
+os.makedirs(pairs_dir, exist_ok = True)
+
+for index, (evil, good) in enumerate(zip(os.listdir("evil"), os.listdir("good"))):
+	pairs = os.path.join(pairs_dir, str(index))
+	os.makedirs(pairs, exist_ok = True)
+
+	subprocess.run([
+		"python3",
+		"collide.py",
+		os.path.join("evil/", evil),
+		os.path.join("good/", good)
+	], check = True)
+	shutil.copyfile(
+		(evil := glob.glob("*-evil.png")[0]),
+		os.path.join(pairs, random_name())
+	)
+	shutil.copyfile(
+		(good := glob.glob("*-good.png")[0]),
+		os.path.join(pairs, random_name())
+	)
+	os.remove(evil)
+	os.remove(good)
